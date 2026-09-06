b04641d3d82c0c1497fc76552011ba3274c73e7d
Shubham <shubhamraj2468sr@gmail.com>
2026-09-07 00:38:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2114,7 +2114,7 @@ function updateWeatherIntelligence() {
     const teamMembers = [
         "SHUBHAM RAJ SHARMA",
         "PAARIJAAT",
-        "RO LI",
+        "RO LI BI ANN",
         "SURAJ KUMAR SINGH",
         "AAKASH YADAV",
         "RISHU RAJ"
@@ -2505,7 +2505,7 @@ function updateWeatherIntelligence() {
     const TEAM = [
         "SHUBHAM RAJ SHARMA",
         "PAARIJAAT",
-        "RO LI",
+        "RO LI BI ANN",
         "SURAJ KUMAR SINGH",
         "AAKASH YADAV",
         "RISHU RAJ"
